ea0c5b964edc997b17582cc89060ad318fa23921
Abhishek <108579783+Abhishek6543jh@users.noreply.github.com>
2023-02-19 19:12:12 +0530

    added new route gradecalculation

--- package/__init__.py
+++ package/__init__.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
-
-
+from flask import session
 app = Flask(__name__)
 app.config['SECRET_KEY']=['myscretkey']
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///users.db'
-app.config['SESSION_PERMANENT'] = True
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+
+
 db = SQLAlchemy(app)
 from package import routes
 
--- package/routes.py
+++ package/routes.py
@@ -3,14 +3,20 @@ from package import *
 from flask import request,render_template,url_for,redirect,flash
 from package.loginandegform import loginform,registrationform
 from package.dbmodels import usrlogin
+app.secret_key="mykey"
+
 with app.app_context():
     db.create_all()
+
 
 #home page
 @app.route("/")
 def home():
-    return render_template('home.html')
-   
+       if not session.get("name"):
+            return redirect(url_for('login'))
+            
+            
+       return render_template('home.html') 
 
 #login page
 @app.route("/login",methods=['POST','GET'])
@@ -23,6 +29,7 @@ def login():
                 login1m = usrlogin.query.filter_by(username=usermail,password=password).first()
                 loginn2m = usrlogin.query.filter_by(email=usermail,password=password).first()
                 if login1m or loginn2m is  not None:
+                    session["name"]=usermail
                     return redirect(url_for('home'))
             
         return render_template('login.html',form=form)
@@ -46,11 +53,18 @@ def reg():
     
 
     return render_template('register.html',form = form)
+
+@app.route("/logout")
+def logout():
+    session["name"]=None
+    return redirect("/")
 #about page
 @app.route("/about")
 def about():
     return render_template('about.html')
-
+@app.route("/gradecalculation")
+def conversion():
+     return render_template('gradecalculation.html')
 if __name__ == "__main__":
     app.run(debug=True)
     
